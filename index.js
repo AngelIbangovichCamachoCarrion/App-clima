5c68cbad4882f3b336118099595cd8ef50bad737
AngelIbangovichCamachoCarrion <2130361@upv.edu.mx>
2024-11-19 21:35:30 -0600

    probemolo

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
-search.addEventListener('click', ()=>{
-    const APIKey = '22b0b63c4190827ab5cc96cb142f5e76'; 
+search.addEventListener('click', () => {
+
+    const APIKey = '22b0b63c4190827ab5cc96cb142f5e76';
     const city = document.querySelector('.search-box input').value;
 
-    if (city == '')
+    if (city === '')
         return;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=${APIKey}`)
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
+
             if (json.cod === '404') {
                 container.style.height = '400px';
                 weatherBox.style.display = 'none';
@@ -21,6 +24,7 @@ search.addEventListener('click', ()=>{
                 error404.classList.add('fadeIn');
                 return;
             }
+
             error404.style.display = 'none';
             error404.classList.remove('fadeIn');
 
@@ -30,26 +34,31 @@ search.addEventListener('click', ()=>{
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
-            switch(json.weather[0].main){
+            switch (json.weather[0].main) {
                 case 'Clear':
                     image.src = 'images/clear.png';
                     break;
+
                 case 'Rain':
                     image.src = 'images/rain.png';
                     break;
-                case 'Smow':
+
+                case 'Snow':
                     image.src = 'images/snow.png';
                     break;
-                case 'Cloud':
-                    image.src = 'cloud.png';
+
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
                     break;
+
                 case 'Haze':
-                    image.src = 'images/haze.png';
+                    image.src = 'images/mist.png';
                     break;
-          
+
                 default:
                     image.src = '';
             }
+
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
@@ -61,6 +70,8 @@ search.addEventListener('click', ()=>{
             weatherDetails.classList.add('fadeIn');
             container.style.height = '590px';
 
+
         });
+
 
 });
